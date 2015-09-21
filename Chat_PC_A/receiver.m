6281c5ea8cec93538eca13537fef61dbe71403dd
yuxuanx <xiayuxuan1993@outlook.com>
2015-09-21 21:02:15 +0200

    Delete receiver.m

--- Chat_PC_A/receiver.m
+++
@@ -1,6 +0,0 @@
-function [pack, psd, const, eyed] = receiver(tout,fc)
-% Complete the function and remove the following lines
-disp('Complete the receiver') 
-pack = []; psd = [];  const=[]; eyed = [];
-
-end
