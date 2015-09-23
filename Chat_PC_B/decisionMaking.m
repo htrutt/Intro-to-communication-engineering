392e8a9625d8304b2a77f40a5116f6a410785df2
yuxuanx <xiayuxuan1993@outlook.com>
2015-09-23 16:55:34 +0200

    Update decisionMaking.m
    replace some k with i

--- Chat_PC_B/decisionMaking.m
+++ Chat_PC_B/decisionMaking.m
@@ -12,14 +12,14 @@ s = [(1 + 1i) (1 - 1i) (-1 + 1i) (-1 - 1i)]/sqrt(2);% Constellation 1 - QPSK/4-Q
 Ifinal=zeros(length(mf_downsample));
 Qfinal=zeros(length(mf_downsample));
     for i=1:length(mf_downsample)
-        D1=norm(s(1)-mf_downsample(k));%Calculate euclidean distance to each
-        D2=norm(s(2)-mf_downsample(k));%point of our constellation
-        D3=norm(s(3)-mf_downsample(k));
-        D4=norm(s(4)-mf_downsample(k));
+        D1=norm(s(1)-mf_downsample(i));%Calculate euclidean distance to each
+        D2=norm(s(2)-mf_downsample(i));%point of our constellation
+        D3=norm(s(3)-mf_downsample(i));
+        D4=norm(s(4)-mf_downsample(i));
         D=[D1 D2 D3 D4]; %Put all the distance in one vector
         [~, I]=min(D);   %Search for the index of the smallest value
-        Ifinal(k)=real(s(I)); %And use this index to determine which symbol was send
-        Qfinal(k)=imag(s(I));
+        Ifinal(i)=real(s(I)); %And use this index to determine which symbol was send
+        Qfinal(i)=imag(s(I));
     end
 
 end
