7bc805069c14e86f411190f3a7e9c7c2bf170fce
htrutt <herve.trutt@gmail.com>
2015-10-07 16:50:15 +0200

    Update generate_modulated_signal

--- Chat_PC_A/generate_modulated_signal.m
+++ Chat_PC_A/generate_modulated_signal.m
@@ -1,4 +1,4 @@
-function [ signal_modulated ] = generate_modulated_signal( fc, sequence, rb )
+function [ signal_modulated ] = generate_modulated_signal( fc, sequence, rb, RC_puls )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 fs = 12e3;                                   % sampling frequency [Hz]
@@ -7,7 +7,6 @@ m = log2(M);                                 % Number of bits per symbol
 rs = rb/m;                                   % Symbol rate
 fsrs = fs/rs;                                % Number of samples per symbol (choose fs such that fsfd is an integer for simplicity)
 
-RC_puls = rtrcpuls(0.4,1/rs,fs,6);           % Pulse shaping function
 x_upsample = bits2symbols(sequence, fsrs, m);   % Bits to symbols
 pulse_tr_RC_samp = pulseShaping(RC_puls, x_upsample, fsrs, fs); % Pulse shaping
 signal_modulated = baseband2passband(pulse_tr_RC_samp ,fc, fs); % Modulation
